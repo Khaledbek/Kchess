4675714e4c70535a85c1daf42cfce7d92fa0655b
AliWatfa1 <153512058+AliWatfa1@users.noreply.github.com>
2026-09-12 23:18:43 +0200

    Create test_hash.py

---
+++ tools/test_hash.py
@@ -0,0 +1,6 @@
+﻿import chess
+import chess.polyglot
+
+board = chess.Board("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+h = chess.polyglot.zobrist_hash(board)
+print(f"Hash: {h}")
